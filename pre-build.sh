abef78847d764a394f86dbc06c968d14663b5aff
Kiran Baddi <kiran_baddi@hotmail.com>
2023-01-12 20:37:26 +0100

    running AZ CLI

--- pre-build.sh
+++ pre-build.sh
@@ -1,58 +1,45 @@
 #!/bin/bash
 
-# ENV=$1
-# TF_STATE_RG=$2
-# TF_STORAGE_ACCOUNT_PREFIX=$3
-# LOCATION=$4
-# TF_CONTAINER="tfstate"
-# TF_KEY="tfstate-$ENV"
+# Check if Resource Group exists if not create resource group
+if  $(az group exists --name $TF_STATE_RG)
+then
+    echo "$TF_STATE_RG resource group exists"
+else
 
-# print temp env variables
-
-echo $TF_STATE_RG $ENV $LOCATION
-
-echo "Completed for now"
-
-# # Check if Resource Group exists if not create resource group
-# if  $(az group exists --name $TF_STATE_RG)
-# then
-#     echo "$TF_STATE_RG resource group exists"
-# else
-
-#     echo "$TF_STATE_RG  doesn't exist creating one"
-#     az group create --name $TF_STATE_RG --location $LOCATION
-# fi
+    echo "$TF_STATE_RG  doesn't exist creating one"
+    az group create --name $TF_STATE_RG --location $LOCATION
+fi
 
 
-# # Check if Storage Account exists, if not create
+# Check if Storage Account exists, if not create
 
-# isSA=$(az storage account list --resource-group $TF_STATE_RG --query "[].name" -otsv | grep -i $TF_STORAGE_ACCOUNT_PREFIX )
+isSA=$(az storage account list --resource-group $TF_STATE_RG --query "[].name" -otsv | grep -i $TF_STORAGE_ACCOUNT_PREFIX )
 
-# echo $isSA
+echo $isSA
 
-# if [[ -z $isSA ]]
-# then
-#     rndstr=$(sed "s/[^a-z0-9]//g" <<< $(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%*()-+' | fold -w 32 | head -n 1  ) | head -c 4)
-#     STORAGEACCOUNT=${TF_STORAGE_ACCOUNT_PREFIX}$rndstr
-#     echo -e "Storage account with the prefix $TF_STORAGE_ACCOUNT_PREFIX doesn't exist.....  \nCreating $STORAGEACCOUNT for the state "
-#     az storage account create --name $STORAGEACCOUNT \
-#                           --resource-group $TF_STATE_RG  \
-#                           --location $LOCATION --sku Standard_LRS 
+if [[ -z $isSA ]]
+then
+    rndstr=$(sed "s/[^a-z0-9]//g" <<< $(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%*()-+' | fold -w 32 | head -n 1  ) | head -c 4)
+    STORAGEACCOUNT=${TF_STORAGE_ACCOUNT_PREFIX}$rndstr
+    echo -e "Storage account with the prefix $TF_STORAGE_ACCOUNT_PREFIX doesn't exist.....  \nCreating $STORAGEACCOUNT for the state "
+    az storage account create --name $STORAGEACCOUNT \
+                          --resource-group $TF_STATE_RG  \
+                          --location $LOCATION --sku Standard_LRS 
 
-# else
-#     echo -e "Storage acocunt with prefix $TF_STORAGE_ACCOUNT_PREFIX exists....\nUsing Storage Account $isSA for the TF state"
-#     STORAGEACCOUNT=$isSA 
+else
+    echo -e "Storage acocunt with prefix $TF_STORAGE_ACCOUNT_PREFIX exists....\nUsing Storage Account $isSA for the TF state"
+    STORAGEACCOUNT=$isSA 
     
-# fi
+fi
 
-# echo "Creating Container "                 
-# az storage container create --name $TF_CONTAINER \
-#                             --account-name $STORAGEACCOUNT 2> /dev/null
+echo "Creating Container "                 
+az storage container create --name $TF_CONTAINER \
+                            --account-name $STORAGEACCOUNT 2> /dev/null
 
-# ACCOUNT_KEY=$(az storage account keys list --resource-group $TF_STATE_RG --account-name $STORAGEACCOUNT --query '[0].value' -o tsv)
-# export ARM_ACCESS_KEY=$ACCOUNT_KEY
+ACCOUNT_KEY=$(az storage account keys list --resource-group $TF_STATE_RG --account-name $STORAGEACCOUNT --query '[0].value' -o tsv)
+export ARM_ACCESS_KEY=$ACCOUNT_KEY
 
-# sed -e "s,#TF_STATE_RG#,$TF_STATE_RG,g" -e "s,#STORAGEACCOUNT#,$STORAGEACCOUNT,g" \
-#     -e "s,#TF_CONTAINER#,${TF_CONTAINER},g" -e "s,#TF_KEY#,${TF_KEY},g" TF/remotebackend.tpl > TF/remotebackend.tf
+sed -e "s,#TF_STATE_RG#,$TF_STATE_RG,g" -e "s,#STORAGEACCOUNT#,$STORAGEACCOUNT,g" \
+    -e "s,#TF_CONTAINER#,${TF_CONTAINER},g" -e "s,#TF_KEY#,${TF_KEY},g" TF/remotebackend.tpl > TF/remotebackend.tf
 
 
