08a31d71927411c44af429d0b6effc391170b75f
Kiran Baddi <kiran_baddi@hotmail.com>
2023-01-12 23:23:28 +0100

    updated random string generator to a simplercmd

--- pre-build.sh
+++ pre-build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sed "s/[^a-z0-9]//g" <<< $(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%*()-+' | fold -w 32 | head -n 1  ) | head -c 4
-
 # # Check if Resource Group exists if not create resource group
 # if  $(az group exists --name $TF_STATE_RG)
 # then
@@ -15,8 +13,8 @@ sed "s/[^a-z0-9]//g" <<< $(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%*()-+' | fol
 
 # az storage account create --name yxyuioysd37888 --resource-group $TF_STATE_RG --location $LOCATION 
 
-# rndstr=$(sed "s/[^a-z0-9]//g" <<< $(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%*()-+' | fold -w 32 | head -n 1  ) | head -c 4)
-# echo $rndstr
+rndstr=$(tr -dc a-z0-9 </dev/urandom | head -c 5 ; echo '')
+echo $rndstr
 
 
 # Check if Storage Account exists, if not create
