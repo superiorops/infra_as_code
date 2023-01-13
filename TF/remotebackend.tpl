441f88969e6e8ebdf2f2c210c048947b2ea37484
Kiran Baddi <kiran_baddi@hotmail.com>
2023-01-13 12:01:46 +0100

    updated Stroage account var

--- TF/remotebackend.tpl
+++ TF/remotebackend.tpl
@@ -2,7 +2,7 @@ terraform {
 
     backend "azurerm" {
         resource_group_name  = "#TF_STATE_RG#"
-        storage_account_name = "#TF_STORAGE_ACCOUNT_PREFIX#"
+        storage_account_name = "#TF_STORAGE_ACCOUNT#"
         container_name       = "#TF_CONTAINER#"
         key                  = "#TF_KEY#"
     }
--- pre-build.sh
+++ pre-build.sh
@@ -18,29 +18,29 @@ isSA=$(az storage account list --resource-group $TF_STATE_RG --query "[].name" -
 if [[ -z $isSA ]]
 then
     rndstr=$(tr -dc a-z0-9 </dev/urandom | head -c 5 ; echo '')
-    STORAGE_ACCOUNT=${TF_STORAGE_ACCOUNT_PREFIX}$rndstr
-    echo -e "Storage account with the prefix $TF_STORAGE_ACCOUNT_PREFIX doesn't exist.....  \nCreating $STORAGE_ACCOUNT for the state "
-    az storage account create --name $STORAGE_ACCOUNT \
+    TF_STORAGE_ACCOUNT=${TF_STORAGE_ACCOUNT_PREFIX}$rndstr
+    echo -e "Storage account with the prefix $TF_STORAGE_ACCOUNT_PREFIX doesn't exist.....  \nCreating $TF_STORAGE_ACCOUNT for the state "
+    az storage account create --name $TF_STORAGE_ACCOUNT \
                           --resource-group $TF_STATE_RG  \
                           --location $LOCATION --sku Standard_LRS 
 
 else
     echo -e "Storage acocunt with prefix $TF_STORAGE_ACCOUNT_PREFIX exists....\nUsing Storage Account $isSA for the TF state"
-    STORAGE_ACCOUNT=$isSA 
+    TF_STORAGE_ACCOUNT=$isSA 
     
 fi
 
 echo "Creating Container "                 
 az storage container create --name $TF_CONTAINER \
-                            --account-name $STORAGE_ACCOUNT 2> /dev/null
+                            --account-name $TF_STORAGE_ACCOUNT 2> /dev/null
 
-ACCOUNT_KEY=$(az storage account keys list --resource-group $TF_STATE_RG --account-name $STORAGE_ACCOUNT --query '[0].value' -o tsv)
+ACCOUNT_KEY=$(az storage account keys list --resource-group $TF_STATE_RG --account-name $TF_STORAGE_ACCOUNT --query '[0].value' -o tsv)
 export ARM_ACCESS_KEY=$ACCOUNT_KEY
 
 TF_KEY="tfstate-$ENV"
 
 
-sed -e "s,#TF_STATE_RG#,$TF_STATE_RG,g" -e "s,#STORAGE_ACCOUNT#,$STORAGE_ACCOUNT,g" \
+sed -e "s,#TF_STATE_RG#,$TF_STATE_RG,g" -e "s,#TF_STORAGE_ACCOUNT#,$TF_STORAGE_ACCOUNT,g" \
     -e "s,#TF_CONTAINER#,$TF_CONTAINER,g" -e "s,#TF_KEY#,$TF_KEY,g" TF/remotebackend.tpl > TF/remotebackend.tf
 
 
